5241790cbd46c27bc0f5d06f98ba8098c70f2893
Brian-thee-shovi <brian14198@gmail.com>
2023-06-06 19:42:51 -0700

    updates

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for ram in range(1,101):
+        if ram % 3 == 0 and ram % 5 == 0:
+            print("FizzBuzz", end=' ')
+        elif ram % 3 == 0:
+            print("Fizz", end=' ')
+        elif ram % 5 == 0:
+            print("Buzz", end=' ')
+        else:
+            print("{:d}".format(ram), end=' ')
