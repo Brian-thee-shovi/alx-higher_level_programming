4d20592ff16400c2d6778e347d9c0debbb731740
Brian-thee-shovi <brian14198@gmail.com>
2023-06-08 15:06:42 -0700

    arguments innit

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """prints the number of and the list of its arguments"""
+    import sys
+
+    list_1 = sys.argv
+    func_len = len(list_1) - 1
+
+    if func_len == 0:
+        print(func_len, "arguments.")
+    elif func_len == 1:
+        print(func_len, "argument:")
+        for ram in range(1, func_len + 1):
+            print("{:d}: {}"format(ram, list_1[ram]))
+    elif func_len > 1:
+        print(func_len, "arguments:")
+        for zi in range(1, func_len + 1):
+            print("{:d}: {}".format(zi, list_1[zi]))
