05ca59acb708dc10eb1c5f2f4109e8ec9af39b6e
Brian-thee-shovi <brian14198@gmail.com>
2023-06-08 15:40:18 -0700

    5-variable_load.py commit

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+
 from variable_load_5 import a
-""" imports the variable a from the file 
-variable_load_5.py and prints its value. """
+"""imports the variable a from the file 
+variable_load_5.py and prints its value."""
 
 if __name__ == "__main__":
     print(a)
