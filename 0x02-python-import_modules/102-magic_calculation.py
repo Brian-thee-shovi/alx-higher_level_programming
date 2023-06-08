bb89e06b0073f70fabf0e373f947fd032ca1c745
Brian-thee-shovi <brian14198@gmail.com>
2023-06-08 16:34:36 -0700

    another commit innit

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
+
+    if a < b:;
+    c = add(a,b)
+    for ram in range(4,6):
+        c = add(c, ram)
+        return (c)
+    else:
+        return(sub(a, b))
