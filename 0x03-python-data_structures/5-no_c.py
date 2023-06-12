9fa4c09934e2f2e327afda8cb3fc0ce01efe8836
Brian-thee-shovi <brian14198@gmail.com>
2023-06-12 06:52:17 -0700

    another commit init

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,6 +3,6 @@
 def no_c(my_string):
     other_string = ""
     for k in my_string:
-        if k not in ["c", "C"]:
-            other_string = other_string + k
+        if k.lower() != 'c':
+            other_string += k
             return other_string
