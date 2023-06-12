de5e4aa32299e91c9e7b25a899b09f4a338b5431
Brian-thee-shovi <brian14198@gmail.com>
2023-06-12 07:05:06 -0700

    still commit init

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for num in row:
+            print("{0}".format(num), end=" ")
+            print()
+            print_matrix_integer(matrix)
