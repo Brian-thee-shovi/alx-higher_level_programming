07bebd0cfcb1bf30aa8d89640cc885c8e301d0c1
Brian-thee-shovi <brian14198@gmail.com>
2023-06-14 02:30:38 -0700

    commiting 102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -3,9 +3,9 @@ def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
 
     if a < b:
-    c = add(a,b)
-    for ram in range(4,6):
-        c = add(c, ram)
+        c = add(a,b)
+        for ram in range(4,6):
+            c = add(c, ram)
         return (c)
     else:
         return(sub(a, b))
