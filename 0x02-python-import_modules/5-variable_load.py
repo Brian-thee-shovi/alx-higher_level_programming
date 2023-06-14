aac6e817ef5bedc44bd91a0aa5d6a40299045da0
Brian-thee-shovi <brian14198@gmail.com>
2023-06-14 02:57:53 -0700

    commiting 5-variable_load.py

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
 from variable_load_5 import a
-"""imports the variable a from the file 
-variable_load_5.py and prints its value."""
 
 if __name__ == "__main__":
     print(a)
