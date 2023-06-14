7162fac8a7b91c09d081dd4e47633675aed14d85
Brian-thee-shovi <brian14198@gmail.com>
2023-06-14 11:35:18 -0700

    commit 12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+    Rom_numeral = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50,
+            'C': 100, 'D': 500, 'M': 1000
+            }
+    sol = 0
+    previous_v = 0
+
+    for numeral i reversed(roman_string):
+        present_v = Rom_ umerall.get(numeral, 0)
+
+        if present_v >= previous_v:
+            sol = sol + present_v
+        else:
+            sol = sol - present_v
+        previous_v = present_v
+    return sol
