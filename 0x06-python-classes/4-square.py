e788086549e1451b7e0654aaaa1df821f847c321
Brian-thee-shovi <brian14198@gmail.com>
2023-06-27 18:02:19 -0700

    4-square.py pytho innit

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+define module documentation
+"""
+
+
+class Square():
+    """
+    class that reps a square
+    """
+
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+        @property
+        def size(self):
+            return self.__size
+
+        @size.setter
+        def size(self, value):
+            if not isinstance(value, int):
+                raise TypeError("size must be an integer")
+            if value < 0:
+                raise ValueError("size must be >= 0")
+
+            self.__size = value
+
+            def area(self):
+                """
+                cal and return the area of a square
+                """
+                return self.__size ** 2
