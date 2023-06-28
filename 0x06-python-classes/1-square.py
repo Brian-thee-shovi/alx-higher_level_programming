96d7aeadc6b0417427f9f3bfe47ccb5ec91d2ffa
Brian-thee-shovi <brian14198@gmail.com>
2023-06-27 17:04:56 -0700

    class Square that defines a square innit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+define module documentation
+"""
+
+
+class Square():
+    """
+    its class that reps a square
+    """
+
+    def __init__(self, size):
+        """
+        Initialize another square.
+
+        Args:
+            size (int): size of other square
+        """
+        self.__size = size
