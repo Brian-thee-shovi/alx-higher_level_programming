d6ba6f9b2b100bb4554637e8082951b47dea78ba
Brian-thee-shovi <brian14198@gmail.com>
2023-06-28 13:45:35 -0700

    elements of a list and only integers

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,5 +7,5 @@ def safe_print_list_integers(my_list=[], x=0):
             num_element += 1
         except(ValueError, TypeError):
             pass
-        print()
-        return num_element
+    print()
+    return num_element
