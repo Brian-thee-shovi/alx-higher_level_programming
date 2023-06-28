d728bb48a38d967ee97a8446dc9df00dabd81d23
Brian-thee-shovi <brian14198@gmail.com>
2023-06-28 13:56:01 -0700

    func that divides 2 integers and prints the result innit

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    division_d = 0
+
+    try:
+        division_d = a / b
+    except ZeroDivisionError:
+        division_d = None
+    finally:
+        print("Inside division_d: {}".format(division_d))
+        return division_d
