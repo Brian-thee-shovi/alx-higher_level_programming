2452ea79828aaeba9f7fc94b3688de38190f5643
Brian-thee-shovi <brian14198@gmail.com>
2023-06-27 17:36:18 -0700

    python code innit

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+define module documentation
+"""
+
+
+class Square():
+    """
+    class that represents/defines a square
+    """
+
+    def __init__(self, size=0):
+        """
+        initialize another square instance
+
+        Args:
+        size (int): its size of the other square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+        def area(self):
+            """
+            cal and return the current area of the square
+            """
+            return self.__size ** 2
