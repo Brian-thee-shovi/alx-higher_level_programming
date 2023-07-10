e20fa67d7935f6d8ff5f17e83b581614986b4c9a
Brian-thee-shovi <brian14198@gmail.com>
2023-07-10 14:48:25 -0700

    6-base_geometry.py python innit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""defines mods documentation"""
+
+
+class BaseGeometry():
+    """reps the class basegeometry"""
+    def area(self):
+        raise Exception("area() is not implemented")
