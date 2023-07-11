a249948952fda99e9a22dbe7075cd0558fcc4756
Brian-thee-shovi <brian14198@gmail.com>
2023-07-11 08:41:38 -0700

    7-add_item.py innit

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+mods documentation for adding argrs to a 
+python list and saving them in a file
+"""
+
+
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load-from_json_file').load_from_json_file
+
+try:
+    data_t = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    data_t = []
+data_t.extend(sys.argv[1:])
+save_to_json_file(data_t, "add_item.json")
