1f49e8dd84e9353ac707ddfc2781feaacf9466bd
Brian-thee-shovi <brian14198@gmail.com>
2023-07-11 08:42:55 -0700

    7-add_item.py innit

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,8 +10,8 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load-from_json_file').load_from_json_file
 
 try:
-    data_t = load_from_json_file("add_item.json")
+    data = load_from_json_file("add_item.json")
 except FileNotFoundError:
-    data_t = []
-data_t.extend(sys.argv[1:])
-save_to_json_file(data_t, "add_item.json")
+    data = []
+data.extend(sys.argv[1:])
+save_to_json_file(data, "add_item.json")
