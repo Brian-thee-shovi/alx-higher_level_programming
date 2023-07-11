406ebaeae1cb4ce0296fbbd3ad95c6027528b0c3
Brian-thee-shovi <brian14198@gmail.com>
2023-07-11 15:29:42 -0700

    Student to JSON with filter innit

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""mod documentation definig a class student"""
+
+
+class Student():
+    def __init__(self, first_name, last_name, age):
+        """initialization of the class/new class"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        returns dict representation of a student
+        """
+        try:
+            for element_t in attrs:
+                if type(element) is not string:
+                    return self.__dict__
+        except Exception:
+            return self.__dict__
+        d_dict = dict()
+        for ky, v_value in self.__dict__.items():
+            if ky in attrs:
+                d_dict[key] = v_value
+        return d_dict
