6b2718779b7708e9e69f2d8ab223773efa33c9c9
Brian-thee-shovi <brian14198@gmail.com>
2023-09-11 19:39:17 -0700

    my commit innit

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const { argv } = require( 'process');
+//func is used to import built-in process module in node.js
+//argv is destructures from the imported process
+//curly braces are part of destructuring syntax
+if (argv.length === 2) {
+  console.log('No argument');
+} else if (argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
