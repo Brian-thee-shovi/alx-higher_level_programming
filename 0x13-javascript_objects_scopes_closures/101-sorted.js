9245f36bd89fcdce25b8d3c4d6e9bf53fe8066da
Brian-thee-shovi <brian14198@gmail.com>
2023-09-12 16:56:21 -0700

    101-sorted.js innit

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const { dict } = require('./101-data);
+
+const my_v = Object.entries(dict).reduce((acc, [key, value]) => {
+  acc[value] = acc[value] ? [...acc[value], key] : [key];
+  return acc;
+}, {});
+console.log(my_v);
