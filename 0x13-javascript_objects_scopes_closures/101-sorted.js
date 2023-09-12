2a6130e8060110ce749dbe49ac085147f62b9f11
Brian-thee-shovi <brian14198@gmail.com>
2023-09-12 16:59:54 -0700

    advanced task innit

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const { dict } = require('./101-data);
+const { dict } = require('./101-data');
 
 const my_v = Object.entries(dict).reduce((acc, [key, value]) => {
   acc[value] = acc[value] ? [...acc[value], key] : [key];
