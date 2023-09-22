cd0e71f24fd7e85486e201364286d7f84137a803
Brian-thee-shovi <brian14198@gmail.com>
2023-09-22 16:16:30 -0700

    lists all states from the database hbtn_0e_0_usa innit

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Module lists states from the database hbtn_0e_0_usa
+"""
+
+if __name__ == '__main__':
+
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(
+        host = 'localhost',
+        port = 3306,
+        usernm=sys.argv[1],
+        passwd=sys.argv[2],
+        datab=sys.argv[3]
+        )
+
+    cursor = datab.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC;")
+    mydata = cursor.fetchall()
+
+    for row in mydata:
+        print(row)
+
+    cursor.close()
+    datab.close()
