dc8e38459ef30dac6d826eb4c812e3fcc5008d00
Brian-thee-shovi <brian14198@gmail.com>
2023-10-24 16:43:52 -0700

    4-starwars_count.js commit

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const myresults = JSON.parse(body).myresults;
+    console.log(myresults.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+	? count + 1
+	: count;
+    }, 0));
+  }
+});
