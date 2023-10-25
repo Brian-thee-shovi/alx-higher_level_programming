edccd2b9b84b6ab950207372964b7b64f7d934fd
Brian-thee-shovi <brian14198@gmail.com>
2023-10-24 17:12:34 -0700

    4-starwars_count.js init

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,13 +1,24 @@
 #!/usr/bin/node
 
 const request = require('request');
-request(process.argv[2], function (error, response, body) {
-  if (!error) {
-    const myresults = JSON.parse(body).myresults;
-    console.log(myresults.reduce((count, movie) => {
-      return movie.characters.find((character) => character.endsWith('/18/'))
-	? count + 1
-	: count;
-    }, 0));
+const myurl = process.argv[2];
+
+request(myurl, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const films = JSON.parse(body).results;
+    let count = 0;
+    for (const eachfilm in films) {
+      const eachfilmchars = films[eachfilm].characters;
+      for (const eachchar in eachfilmchars) {
+	if (eachfilmchars[eachchar]. includes('18')) {
+	  count++;
+	}
+      }
+    }
+    console.log(count);
+  } else {
+    console.log('Status code: ' + response.statusCode);
   }
 });
